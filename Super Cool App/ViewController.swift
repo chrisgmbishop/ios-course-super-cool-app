2638259251c30e937aec9bd652799b513188e74a
Chris Bishop <chrisgmbishop@gmail.com>
2015-12-24 23:15:09 -0330

    Initial Commit

---
+++ Super Cool App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Super Cool App
+//
+//  Created by Chris Bishop on 2015-12-24.
+//  Copyright © 2015 Chris Bishop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Super Cool AppTests/Super_Cool_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Super_Cool_AppTests.swift
+//  Super Cool AppTests
+//
+//  Created by Chris Bishop on 2015-12-24.
+//  Copyright © 2015 Chris Bishop. All rights reserved.
+//
+
+import XCTest
+@testable import Super_Cool_App
+
+class Super_Cool_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
